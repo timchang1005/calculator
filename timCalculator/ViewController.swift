aada21a3b104934ab2ec818bec98a3c34b96d3c2
YiChun <ycycchre@gmail.com>
2019-04-30 19:30:44 +0800

    first calculator

---
+++ timCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timCalculator
+//
+//  Created by Tomoki Morimoto on 2019/4/30.
+//  Copyright © 2019 Tomoki Morimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
